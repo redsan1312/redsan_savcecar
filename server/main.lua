96bf5dbd72f0f59f298fc638fba293ea03ee1013
redsan <official.redsan@gmail.com>
2024-09-18 20:49:17 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -17,6 +17,12 @@ local function generateRandomPlate()
     return plate
 end
 
+local function isPlateTaken(plate, callback)
+    MySQL.query('SELECT 1 FROM owned_vehicles WHERE plate = ?', {plate}, function(result)
+        callback(#result > 0)
+    end)
+end
+
 ESX.RegisterCommand('givecar', Config.AdminGroups, function(xPlayer, args, showError)
     local targetPlayer = ESX.GetPlayerFromId(args.playerId)
     local vehicleModel = args.model
@@ -35,43 +41,52 @@ ESX.RegisterCommand('givecar', Config.AdminGroups, function(xPlayer, args, showE
         return showError(errorMsg)
     end
 
-    local playerPed = GetPlayerPed(targetPlayer.source)
-    local vehicle = GetVehiclePedIsIn(playerPed, false)
-    if not vehicle then
-        return showError(_U('not_in_vehicle'))
-    end
+    plate = plate or generateRandomPlate()
 
-    local vehicleProps = {
-        model = GetHashKey(vehicleModel),
-        plate = plate
-    }
-
-    local existingVehicle = MySQL.query.await('SELECT * FROM owned_vehicles WHERE plate = ?', {plate})
-
-    if existingVehicle[1] then
-        MySQL.update.await('UPDATE owned_vehicles SET model = ?, owner = ?, vehicle = ? WHERE plate = ?', {
-            vehicleModel,
-            targetPlayer.getIdentifier(),
-            json.encode(vehicleProps),
-            plate
-        })
-    else
-        MySQL.insert.await('INSERT INTO owned_vehicles (model, owner, plate, vehicle, type) VALUES (?, ?, ?, ?, ?)', {
-            vehicleModel,
-            targetPlayer.getIdentifier(),
-            plate,
-            json.encode(vehicleProps),
-            'car'
-        })
-    end
-
-    xPlayer.showNotification(_U('givecar_success', args.playerId))
-    targetPlayer.showNotification(_U('receive_car', vehicleModel, plate))
+    isPlateTaken(plate, function(plateTaken)
+        if plateTaken then
+            MySQL.update('UPDATE owned_vehicles SET model = ?, owner = ?, plate = ? WHERE plate = ?', {
+                vehicleModel,
+                targetPlayer.getIdentifier(),
+                plate
+            }, function(affectedRows)
+                if affectedRows > 0 then
+                    xPlayer.showNotification(_U('givecar_success', args.playerId))
+                    targetPlayer.showNotification(_U('receive_car', vehicleModel, plate))
+                else
+                    showError(_U('vehicle_update_failed'))
+                end
+            end)
+        else
+            MySQL.insert('INSERT INTO owned_vehicles (model, owner, plate, vehicle, type) VALUES (?, ?, ?, ?, ?)', {
+                vehicleModel,
+                targetPlayer.getIdentifier(),
+                plate,
+                '{}',
+                'car'
+            }, function(insertId)
+                if insertId then
+                    xPlayer.showNotification(_U('givecar_success', args.playerId))
+                    targetPlayer.showNotification(_U('receive_car', vehicleModel, plate))
+                else
+                    showError(_U('vehicle_insert_failed'))
+                end
+            end)
+        end
+    end)
 end, true, {help = _U('givecar'), arguments = {
     {name = 'playerId', help = _U('player_id'), type = 'number'},
     {name = 'model', help = _U('vehicle_model'), type = 'string'},
     {name = 'plate', help = _U('plate'), type = 'string', optional = true}
 }})
+
+RegisterNetEvent('esx_giveownedcar:setVehicleProperties')
+AddEventHandler('esx_giveownedcar:setVehicleProperties', function(vehicleProps, plate)
+    MySQL.update('UPDATE owned_vehicles SET vehicle = ? WHERE plate = ?', {
+        json.encode(vehicleProps),
+        plate
+    })
+end)
 
 ESX.RegisterCommand('savecar', 'user', function(xPlayer)
     local playerPed = GetPlayerPed(xPlayer.source)
@@ -82,16 +97,24 @@ ESX.RegisterCommand('savecar', 'user', function(xPlayer)
     end
 
     local plate = GetVehicleNumberPlateText(vehicle)
-    local vehicleProps = ESX.Game.GetVehicleProperties(vehicle)
+    local vehicleProps = {
+        model = GetEntityModel(vehicle)wwwww,
+        plate = plate,
+        state = {}
+    }
 
-    MySQL.insert.await('INSERT INTO owned_vehicles (model, owner, plate, vehicle, type, state) VALUES (?, ?, ?, ?, ?, ?)', {
+    MySQL.insert('INSERT INTO owned_vehicles (model, owner, plate, vehicle, type, state) VALUES (?, ?, ?, ?, ?, ?)', {
         vehicleProps.model,
         xPlayer.getIdentifier(),
         plate,
         json.encode(vehicleProps),
         'car',
         json.encode(vehicleProps.state or {})
-    })
-
-    xPlayer.showNotification(_U('savecar_success', plate))
+    }, function(insertId)
+        if insertId then
+            xPlayer.showNotification(_U('savecar_success', plate))
+        else
+            xPlayer.showNotification(_U('savecar_failed'))
+        end
+    end)
 end, false, {help = _U('save_command_car')})
